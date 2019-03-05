b9489d4aab1322b2a7e81b7c62d2ebbd1c2eed70
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2019-03-05 13:13:49 -0600

    add gridftp container

---
+++ gridftp/build_container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t vbrik/gridftp:latest .
---
+++ gridftp/run_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p ~/.container_histories
+touch ~/.container_histories/vbrik-gridftp
+
+docker run -it --rm --name vbrik-gridftp \
+        -v ~/.container_histories/vbrik-gridftp:/root/.bash_history \
+        -v ~/.globus:/root/.globus \
+        vbrik/gridftp:latest
